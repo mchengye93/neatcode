ae5515da74a4cb012c24120da104aef2b035499b
Marbo Cheng Ye <marbochengye@gmail.com>
2022-05-15 12:04:14 -0700

    reverse char problem

---
+++ Cake/ArraysandString/ReverseCharacters/reverseChar.py
@@ -0,0 +1,13 @@
+def reverse(word):
+	left = 0
+	right = len(word) - 1
+
+	while left < right:
+		word[left], word[right] = word[right], word[left]
+
+		left +=1
+		right -=1
+
+	return word
+
+print(reverse(['h','e','l','l','o']))
